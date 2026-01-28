8c1d74a082f982337e80f375d365780dddfa27fc
AidysM <mongush10011979@gmail,com>
2026-01-28 22:13:41 +0300

    added follow system

--- socialws/settings.py
+++ socialws/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+from django.urls import reverse_lazy
 from pathlib import Path
 from decouple import config
 import os
@@ -161,3 +162,7 @@ SOCIAL_AUTH_PIPELINE = [
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 ]
+
+ABSOLUTE_URL_OVERRIDES = {
+    'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
+}
